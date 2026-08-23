a4cdc996b6c3a46e05d1f1e10b9d5535b7717277
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-08-24 01:24:36 +0530

    Add Python_all_concept

---
+++ Python_all_concept/variable_operator.py
@@ -0,0 +1,46 @@
+#! DAY-1 BASIC AND TYPE
+
+print("This include all python concept with imp ques")
+
+#@ this is modulus give us remainder
+print(3%2)
+
+#@ this is floor division operator 1.5 into 1
+print(3 // 2)
+
+#@ list - dynamic can change can store easily(store ref/pointer)
+print(type([23,34,6]))
+#% name.append("name we want to add")  to add stuff
+
+#@tuple - fix cant cahnge take low memory
+print(type((4,5,6,))) #type like that to get type
+
+#@set - fix cant cahnge take low memory
+print(type({4,5,6,})) #type like that to get type
+
+#@dictionary  key:value
+print({'Name': 'Chirag'})
+
+#! DAY - 2 VARIABLE
+
+#@ Input from user
+#* Write like that (name =) then put in print 
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+city = input("Enter your city: ")
+
+print(name)
+print(age)
+print(city)
+
+#@ CONVERTING ONE TYPE TO ANOTHER
+#% new name = type which it going to change(old name)
+age_float = float(age)
+
+#@ length of string
+print(len(name))
+
+#@ max number (we will create and it will findout max number)
+number = 3,27,7365
+print(max(number))
+
