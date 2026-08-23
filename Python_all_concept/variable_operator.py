82ad21ffe0fd8bd770cca50b419357f50e5c510c
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-08-24 01:43:10 +0530

    Operator

--- Python_all_concept/variable_operator.py
+++ Python_all_concept/variable_operator.py
@@ -1,3 +1,5 @@
+#  python "Python_all_concept\variable_operator.py"
+
 #! DAY-1 BASIC AND TYPE
 
 print("This include all python concept with imp ques")
@@ -7,6 +9,7 @@ print(3%2)
 
 #@ this is floor division operator 1.5 into 1
 print(3 // 2)
+#% means go to smaller   alert when negative number
 
 #@ list - dynamic can change can store easily(store ref/pointer)
 print(type([23,34,6]))
@@ -26,10 +29,10 @@ print({'Name': 'Chirag'})
 #@ Input from user
 #* Write like that (name =) then put in print 
 name = input("Enter your name: ")
-age = int(input("Enter your age: "))
+age = int(input("Enter your age: ")) #* use int and float
 city = input("Enter your city: ")
 
-print(name)
+print("Name: ", name)
 print(age)
 print(city)
 
@@ -44,3 +47,25 @@ print(len(name))
 number = 3,27,7365
 print(max(number))
 
+#! DAY 3 OPERATOR
+#? BASIC CAL
+number = int(input("Whats the Number: "))
+square = number**2
+cube = number**3
+division = number/2
+floor = number//2
+reminder = number % 2
+print("Division: ", division)
+print("Floor: ", floor)
+print("Remainder: ", reminder)
+
+#@ AND - * both follow
+#@ OR - +  only one follow
+#@ NOT     reverse result
+
+#@ in   Bollen true or false
+print("Does your name contain 'a'? ", "a" in name)
+print("Does your name contain 'on'? ", "on" in name)
+
+#@compare
+print(5 > 2)
