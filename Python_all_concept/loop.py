1e9f11c9099feb4a73e53fe5a327eadb4fda3ea5
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-08-25 01:48:18 +0530

    loop ques

---
+++ Python_all_concept/loop.py
@@ -0,0 +1,23 @@
+# python "Python_all_concept\loop.py"
+
+def program1():
+    n = int(input("Whats n: "))
+    count = 0
+    while count < n: # going to print atleast one time
+        print("3")
+        count += 1
+        if n == 3:  # if input
+            break    
+
+def program3():
+ numbers = [1,2,3,4]
+
+ for num in numbers:
+    print(num)   
+
+def program4():
+ for i in  range(0,10,3):
+   print(i)            
+
+program4()        
+    
