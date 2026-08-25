22cba6dd71e7520b2ac816eb2b780b2d7972d737
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-08-25 15:14:46 +0530

    Stat and Random module

--- Python_all_concept/Modues.py
+++ Python_all_concept/Modues.py
@@ -42,7 +42,31 @@ print(sys.path)
 
 print("Hello")
 
-sys.exit()
+#sys.exit()  #& it exit everything after that
 
 print("World")
 
+#! Statistics, math and random Module
+
+import statistics as st
+print(st.stdev([200,390])) #! see carefully
+print(st.mode([1,1,1,200, 200,200,39]))
+
+
+import math as m
+print(m.pi)
+print(int(m.pow(2,4))) #! see here
+print(m.floor(2.45))
+print(m.ceil(2.45))
+
+print(dir(m))
+
+
+import random as r
+print(r.random()) # will print out between 0 to 1print(r.random(1,10))
+
+colour = ['red','green','blue']
+print(r.choice(colour))
+
+card = ['diamond','king','heart','queen','joker']
+print(r.shuffle(card)) # it can give us none
