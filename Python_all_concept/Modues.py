4bfd0bd9b969d10e2e12a005326167b9c7a61ee0
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-08-25 12:06:46 +0530

    Module simple calculator

---
+++ Python_all_concept/Modues.py
@@ -0,0 +1,15 @@
+#! DAY - 12  MODULES
+# python "Python_all_concept\Modues.py"
+
+# this is module   just .py file
+def add(a, b,c):
+    return a + b + c
+
+def sub(a, b, c ):
+    return a - b
+
+PI = 3.14159
+
+print(add(5,4,6))
+print(sub(5,4,6))
+print(add(5,4,6)*PI)
