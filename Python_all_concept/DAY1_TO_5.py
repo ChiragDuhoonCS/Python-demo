f80f0d456934d6a4989d28fd904efe8ea19f9b34
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-08-26 22:47:01 +0530

    from 1 to 5 ques

---
+++ Python_all_concept/DAY1_TO_5.py
@@ -0,0 +1,29 @@
+"""
+Exercise: Student Profile Card
+
+Write a Python program that:
+
+1. Takes input for:
+   - Name
+   - Age
+   - City
+   - Favorite Programming Language
+   - 3 Favorite Fruits (store them in a list)
+
+2. Prints a formatted profile card.
+
+3. Displays:
+   - Name in uppercase and lowercase
+   - Length of the name
+   - Reverse of the name
+   - Age after 5 years
+   - Birth year (Assume current year = 2026)
+   - First and last fruit
+   - Total number of fruits
+   - Check whether "Apple" is in the fruit list
+   - Print the data type of every variable
+
+Rules:
+- Don't use if, for, while, functions, or modules.
+- Use only concepts from Days 1–5.
+"""
